d1cfa13d6f9cd064e013f0e5e84b0af6628f9392
ptabasso2 <pejman.tabassomi@gmail.com>
2022-02-07 11:08:11 +0100

    Base work for activity 03

--- src/main/java/com/datadoghq/pej/Application.java
+++ src/main/java/com/datadoghq/pej/Application.java
@@ -1,12 +1,8 @@
 package com.datadoghq.pej;
 
-import datadog.opentracing.DDTracer;
-import io.opentracing.Tracer;
-import io.opentracing.util.GlobalTracer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
 
@@ -18,10 +14,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public Tracer ddtracer() {
-        Tracer tracer = new DDTracer.DDTracerBuilder().build();
-        GlobalTracer.registerIfAbsent(tracer);
-        return tracer;
-    }
 }
