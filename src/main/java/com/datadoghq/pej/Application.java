a073e925e1da99ff20e080afb255b418a958ccf9
ptabasso2 <pejman.tabassomi@gmail.com>
2022-04-16 15:02:09 +0200

    Mqtt details

--- src/main/java/com/datadoghq/pej/Application.java
+++ src/main/java/com/datadoghq/pej/Application.java
@@ -8,8 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
 import java.io.IOException;
+
 
 @Slf4j
 @SpringBootApplication
